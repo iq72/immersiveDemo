bfed14ca23e4aed9b3001b54578bae3160715863
iq72 <liukyzy008@gmail.com>
2015-09-29 17:33:44 +0800

    useKeydownKeyupAsTrigger

--- js/immersive.js
+++ js/immersive.js
@@ -1,8 +1,31 @@
 function main (){
   console.log("loaded");
-  document.addEventListener('keydown', onKeydown, false)
+  document.addEventListener('keydown', onKeydown, false);
+  document.addEventListener('keyup', onKeyup, false);
 }
 document.addEventListener('DOMContentLoaded', main, false);
+var lastEvent,
+    requestID,
+    loopStoped=true,
+    velocity=0,
+    lastTime=0,
+    duration=0;
+
+// reset to defualt
+function onKeyup(){
+  // console.log("keyup");
+  velocity=0;
+  lastTime = 0;
+  duration = 0;
+
+  // stop the move loop
+  if(requestID){
+    // console.log(requestID);
+    window.cancelAnimationFrame(requestID);
+    requestID = undefined;
+    loopStoped=true;
+  }
+}
 
 function onKeydown(e){
   // console.log(e.keyCode);
@@ -22,11 +45,37 @@ function onKeydown(e){
       break;
     case 38:
       console.log("arrowUp");
-      naviForward();
+      // start the moveForward loop;
+      if(loopStoped){
+        moveForward();
+      }
       break;
     default:
       break;
   }
+}
+
+function moveForward(timestamp) {
+  loopStoped=false;
+  // console.log("requestID is " + requestID);
+  velocity < 1 ? velocity+=0.1 : velocity = 1;
+  if ( 0 === lastTime) {
+    // firstTime
+  }else{
+    // we got a duration
+    duration=timestamp - lastTime;
+    // console.log("time between two frame is " + duration);
+
+    var distance = velocity * duration;
+    var cards=document.querySelectorAll('.center .card');
+
+    for (var i=0; i<cards.length; i++){
+      var card=cards[i];
+      var curZ=card.style.transform
+    }
+  }
+  lastTime = timestamp;
+  requestID = window.requestAnimationFrame(moveForward);
 }
 
 function naviForward() {
