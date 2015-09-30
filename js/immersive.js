b05781ac34cad5ea52dcf5b0a29bc2b718fde59f
iq72 <liukyzy008@gmail.com>
2015-09-30 17:00:47 +0800

    constantVelocityWorks
    but when velocity is variable, it becomes triggy

--- js/immersive.js
+++ js/immersive.js
@@ -9,14 +9,14 @@ var lastEvent,
     loopStoped=true,
     velocity=0,
     lastTime=0,
-    duration=0;
+    interval=0;
 
 // reset to defualt
 function onKeyup(){
   // console.log("keyup");
   velocity=0;
   lastTime = 0;
-  duration = 0;
+  interval = 0;
 
   // stop the move loop
   if(requestID){
@@ -47,7 +47,7 @@ function onKeydown(e){
       console.log("arrowUp");
       // start the moveForward loop;
       if(loopStoped){
-        moveForward();
+          requestID = window.requestAnimationFrame(moveForward);
       }
       break;
     default:
@@ -59,19 +59,67 @@ function moveForward(timestamp) {
   loopStoped=false;
   // console.log("requestID is " + requestID);
   velocity < 1 ? velocity+=0.1 : velocity = 1;
+  // velocity=0.1;
   if ( 0 === lastTime) {
+    // console.log("fist time \n timestamp is " + timestamp);
     // firstTime
+    velocity=0.1;
   }else{
-    // we got a duration
-    duration=timestamp - lastTime;
-    // console.log("time between two frame is " + duration);
+    // we got a interval
+    interval=timestamp - lastTime;
+    // console.log("time between two frame is " + interval);
 
-    var distance = velocity * duration;
+    var distance = velocity * interval;
     var cards=document.querySelectorAll('.center .card');
 
     for (var i=0; i<cards.length; i++){
       var card=cards[i];
-      var curZ=card.style.transform
+
+      // get card's z value
+      var st=window.getComputedStyle(card,null); //get computed style
+      var transform=st.getPropertyValue("transform"); //get transform property
+      var transformValue=transform.split('(')[1].split(')')[0].split(',');// get value, split
+      var scaleX=parseInt(transformValue[0]);
+      var scaleY=parseInt(transformValue[5]);
+      var scaleZ=parseInt(transformValue[10]);
+      var y=parseInt(transformValue[13]) / scaleY;
+      var z=parseInt(transformValue[14]) / scaleZ;// get z value in int
+
+      // different cards with different speed
+    if(z < -200){ // if not the toppist card
+      console.log("NORMAL: \n"+"i= "+i +";    " + "velocity= " + velocity );
+      z = Math.round(z+distance);
+      card.style.transform="translate3d(0px, 0px, " + z +"px)";
+    }else if(-200 <= z && 100 > z){ // speed * 3 for the toppist card to makesure all cards arrive at same time
+      // distance = velocity * 3 * interval;
+      console.log("3X: \n"+"i= "+i +";    " + "velocity= " + velocity );
+      z=Math.round(z+(distance*3));
+      card.style.transform="translate3d(0px, 0px, " + z +"px)";
+
+    }else if (100 <= z && 150 > z) { // another speed and scale and opacity
+      console.log("1/2X: \n"+"i= "+i +";    " + "velocity= " + velocity );
+      // distance = velocity*interval/2;
+
+      // scaleX = scaleY += distance*0.2/50;
+      // scaleZ += distance*0.5/50;
+      z=Math.round(z+ (distance/2));
+      // opacity
+      var opacity=st.getPropertyValue("opacity");
+      opacity=parseFloat(opacity);
+      opacity-=distance/50;
+      // set style
+      // card.style.opacity=opacity;
+      card.style.transform="translate3d(0px, 0px, " + z +"px) "+
+                           "scale3d(" + scaleX +", "+scaleY+", "+scaleZ +")";
+    }else{
+    //  z=-600;
+      card.style.transform="translate3d(0px, 0px, -500px)";
+      card.style.opacity=1;
+
+    }
+    if(i==0){
+      // console.log(card.style.transform);
+    }
     }
   }
   lastTime = timestamp;
