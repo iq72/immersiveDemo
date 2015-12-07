e66b859d9e4a809d5a52da9601ee4779fff18f0a
iq72 <liukyzy008@gmail.com>
2015-12-07 18:24:35 +0800

    setDefaultView

--- immersive/routes/immersive.js
+++ immersive/routes/immersive.js
@@ -3,14 +3,15 @@ var AV = require('leanengine');
 
 // `AV.Object.extend` 方法一定要放在全局变量，否则会造成堆栈溢出。
 // 详见： https://leancloud.cn/docs/js_guide.html#对象
-var Todo = AV.Object.extend('Videos');
+var Videos = AV.Object.extend('Videos');
 
 // 查询 Todo 列表
 router.get('/', function(req, res, next) {
-  var query = new AV.Query(Todo);
+  var query = new AV.Query(Videos);
   query.descending('createdAt');
   query.find({
     success: function(results) {
+      console.log("Results: \n" + results);
       res.render('immersive', {
         videos: results
       });
