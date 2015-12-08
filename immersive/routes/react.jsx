1cf2f30890eb44e38defc97a15dec0107e5bbf3c
iq72 <liukyzy008@gmail.com>
2015-12-08 18:37:59 +0800

    ini

---
+++ immersive/routes/react.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var HelloMessage = React.createClass({
+  render:function(){
+    return <h1> Hello {this.props.name} <h1>;
+  }
+});
+
+React.render(<HelloMessage name="World" />, mountNode);
