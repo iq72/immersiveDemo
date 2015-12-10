df0910b2d1c1d4974107ebd1275f09da90c34131
iq72 <liukyzy008@gmail.com>
2015-12-10 18:03:00 +0800

    stillProblem

--- immersive/public/js/immersiveui.js
+++ immersive/public/js/immersiveui.js
@@ -374,44 +374,44 @@ function rotateStandalize(directon){
   document.querySelectorAll('.collections')[index].classList.add("transition");
 }
 
-function naviForward() {
-  // manipulating the cards UI, by change class to "layer-"+1
-    var cards=document.querySelectorAll('.center .card');
-    // console.log(cards);
-    for (var i=0; i<cards.length; i++){
-      // console.log("change started");
-      var card=cards[i];
-      var curClass="layer-"+i ,
-          newClass;
-      if(i<6){
-        newClass="layer-"+(1+i);
-      }else {
-        newClass="layer-0";
-        document.querySelector('.center').insertBefore(card, document.querySelector(' .layer-1'));
-        // need to rebind layer-0 content
-      }
-      card.classList.remove(curClass);
-      card.classList.add(newClass);
-    }
-}
+// function naviForward() {
+//   // manipulating the cards UI, by change class to "layer-"+1
+//     var cards=document.querySelectorAll('.center .card');
+//     // console.log(cards);
+//     for (var i=0; i<cards.length; i++){
+//       // console.log("change started");
+//       var card=cards[i];
+//       var curClass="layer-"+i ,
+//           newClass;
+//       if(i<6){
+//         newClass="layer-"+(1+i);
+//       }else {
+//         newClass="layer-0";
+//         document.querySelector('.center').insertBefore(card, document.querySelector(' .layer-1'));
+//         // need to rebind layer-0 content
+//       }
+//       card.classList.remove(curClass);
+//       card.classList.add(newClass);
+//     }
+// }
 
-function naviBackward() {
-  // manipulating the cards UI, by change class to "layer-"-1
-    var cards=document.querySelectorAll('.center .card');
-    // console.log(cards);
-    for (var i=0; i<cards.length; i++){
-    //  console.log("change started");
-      var card=cards[i];
-      var curClass="layer-"+i ,
-          newClass;
-      if(i>0){
-        newClass="layer-"+(i-1);
-      }else {
-        newClass="layer-6";
-        document.querySelector('.center').appendChild(card);
-        // need to rebind layer-6 content
-      }
-      card.classList.remove(curClass);
-      card.classList.add(newClass);
-    }
-}
+// function naviBackward() {
+//   // manipulating the cards UI, by change class to "layer-"-1
+//     var cards=document.querySelectorAll('.center .card');
+//     // console.log(cards);
+//     for (var i=0; i<cards.length; i++){
+//     //  console.log("change started");
+//       var card=cards[i];
+//       var curClass="layer-"+i ,
+//           newClass;
+//       if(i>0){
+//         newClass="layer-"+(i-1);
+//       }else {
+//         newClass="layer-6";
+//         document.querySelector('.center').appendChild(card);
+//         // need to rebind layer-6 content
+//       }
+//       card.classList.remove(curClass);
+//       card.classList.add(newClass);
+//     }
+// }
