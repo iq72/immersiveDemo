2133f08b13f004f6fdea74e8a49e032eb7f81cbf
iq72 <liukyzy008@gmail.com>
2015-12-15 14:32:27 +0800

    PassEvents get children from parents using “refs”

--- immersive/public/js/reactImmersiv.jsx
+++ immersive/public/js/reactImmersiv.jsx
@@ -96,11 +96,91 @@ function getPercentage(interval){
 }
 
 var ImsvUI = React.createClass({
+  collectionsCount:8,
+  focusPostition: 1,
+  //hack keydown events
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeydown);
+    document.addEventListener("keyup", this.handleKeyup)
+  },
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeydown);
+    document.removeEventListener("keyup", this.handleKeyup)
+  },
+  handleKeyup: function(e){
+    console.log("KEYUP hacked \n " + e.keyCode);
+    //TODO : reposite cards and collections to grid
+    switch (e.keyCode) {
+      case 13:
+        console.log("enter")
+        break;
+      case 38:
+        console.log("arrowUp");
+        var centerCollection=this.explorer.center;
+        centerCollection.setState({
+          scrollVertical:centerCollection.state.scrollVertical+1
+        });
+        break;
+      case 40:
+        console.log("arrowDown");
+        var centerCollection=this.explorer.center;
+        centerCollection.setState({
+          scrollVertical:centerCollection.state.scrollVertical-1
+        });
+        break;
+      case 37:
+        console.log("arrowLeft");
+        var newScrollHorizontal = this.state.scrollHorizontal + 1;
+        this.setState({
+          scrollHorizontal: newScrollHorizontal
+        });
+        break;
+      case 39:
+        console.log("arrowRight");
+        var newScrollHorizontal = this.state.scrollHorizontal - 1;
+        this.setState({
+          scrollHorizontal: newScrollHorizontal
+        });
+        break;
+      default:
+        break;
+    }
+  },
+  handleKeydown: function(e){
+    console.log("KEYdown hacked \n " + e.keyCode);
+    //TODO : move cards and collections
+    switch (e.keyCode) {
+      case 38:
+        console.log("arrowUp");
+        break;
+      case 40:
+        console.log("arrowDown");
+        break;
+      case 37:
+        console.log("arrowLeft");
+        //move collections clockwise
+        break;
+      case 39:
+        console.log("arrowRight");
+        //move collections anti-clockwise
+        break;
+      default:
+        break;
+    }
+  },
+
+  getInitialState: function(){
+    return {
+      scrollHorizontal:0,
+    };
+  },
   render : function(){
+    var rotate={};
+    rotate.transform= 'rotateY(' + (this.state.scrollHorizontal*360/8) + 'deg)';
     return (
       <div className="imsvUI">
         <Status></Status>
-        <Explorer></Explorer>
+        <Explorer collectionsCount={this.collectionsCount} cardsCount={7} scrollHorizontal={this.state.scrollHorizontal} rotate={rotate} focusPostition={this.focusPostition+this.state.scrollHorizontal}  ref={ref => this.explorer = ref}></Explorer>
         <Inspector></Inspector>
       </div>
     );
@@ -150,46 +230,38 @@ var Catergory = React.createClass({
   }
 });
 
+//
+// Explorer
+//
+
 var Explorer = React.createClass({
-  handleKeyup: function(e){
-    console.log("KEYUP hacked \n " + e.keyCode);
-  },
-  handleKeydown: function(e){
-    console.log("KEYdown hacked \n " + e.keyCode);
-  },
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeydown);
-    document.addEventListener("keyup", this.handleKeyup)
-  },
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeydown);
-    document.removeEventListener("keyup", this.handleKeyup)
-  },
-  getInitialState: function(){
-    return {
-      scrollVertical:0,
-      scrollHorizontal:0,
-    };
+  // getInitialState: function(){
+  //   return{
+  //     scrollVertical:0
+  //   };
+  // },
+  handleMoveCard:function(direction, percentage){
+    console.log("got command to move card: \n"+ direction + percentage);
+    //TODO move cards vertically
   },
   render: function(){
     var catergories = [];
     for(var i=0;i<8;i++){
       var className = "collections";
-      if(1===i){
+      if(this.props.focusPostition===i){
         className += " center";
         catergories.push(
-          <CardCollections catergory="video" className={className} key={i}/>
+          <CardCollections catergory="video" className={className} key={i}  ref={(ref) => this.center = ref}/>
         );
       }else {
         catergories.push(
-          <CardCollections catergory="card" className={className} key={i}/>
+          <CardCollections catergory="card"  className={className} key={i}/>
         );
       }
-
     }
     return (
       <main>
-        <section className="container-3d transition">
+        <section style={this.props.rotate} className="container-3d transition">
           {catergories}
         </section>
       </main>
@@ -200,67 +272,67 @@ var Explorer = React.createClass({
 var CardCollections = React.createClass({
   catergory:"",
   className:"",
-  getInitialState:function(){
+  // handleMove: function(direction, percentage){
+  //   console.log("handleMove trigged");
+  //   percentage = percentage * "forward"==direction? 1 : -1;
+  //   var newOrders=[], newStyles=[];
+  //   for(var i=0; i<7; i++){
+  //     var order=(this.children[i].props.order + percentage)%7;
+  //     var newStyle={};
+  //     var z,y,scaleX,scaleY,opacity;
+  //     //logic for new style
+  //     if(order<4){
+  //       //linear translateZ -600 ~ -200
+  //       z = Math.round(order*100 - 600);
+  //       style.opacity=1;
+  //       style.transform="translate3d(0px, 0px, " + z +"px)";
+  //     }else if (order<5) {
+  //       //linear translateZ -200 ~ 100
+  //       z = Math.round(order*300 - 1400);
+  //       style.opacity=1;
+  //       style.transform="translate3d(0px, 0px, " + z +"px)";
+  //     }else if (order<6) {
+  //       //scale3d + translateZ + opacity
+  //       scaleX = scaleY = (order-5)*0.2 + 1;
+  //       // scaleZ += dz*0.5/50;
+  //       z=Math.round((order-5)*150 + 100);
+  //       y=Math.round((order-5)*260);
+  //       // opacity
+  //
+  //       opacity=1-((order-5)*1.5);
+  //       // if(opacity <= 0){
+  //       //   console.log("z position: "+ z);
+  //       // }
+  //       // set style
+  //       style.opacity=opacity;
+  //       style.transform="translate3d(0px, "+ y +"px, " + z +"px) "+
+  //                            "scale3d(" + scaleX +", "+scaleY+", "+scaleZ +")";
+  //     }else {
+  //       z = Math.round((order-6)*100 + 250);
+  //       style.opacity=0;
+  //       style.transform="translate3d(0px, "+ y +"px, " + z +"px) "+
+  //                            "scale3d(" + scaleX +", "+scaleY+", "+scaleZ +")";
+  //     }
+  //
+  //     newOrders.push(order);
+  //   }
+  // },
+  getInitialState: function(){
     return{
-      orders:[0,1,2,3,4,5,6,7],
-      styles:[{},{},{},{},{},{},{}]
+      scrollVertical:0
     };
-  },
-  handleMove: function(direction, percentage){
-    console.log("handleMove trigged");
-    percentage = percentage * "forward"==direction? 1 : -1;
-    var newOrders=[], newStyles=[];
-    for(var i=0; i<7; i++){
-      var order=(this.children[i].props.order + percentage)%7;
-      var newStyle={};
-      var z,y,scaleX,scaleY,opacity;
-      //logic for new style
-      if(order<4){
-        //linear translateZ -600 ~ -200
-        z = Math.round(order*100 - 600);
-        style.opacity=1;
-        style.transform="translate3d(0px, 0px, " + z +"px)";
-      }else if (order<5) {
-        //linear translateZ -200 ~ 100
-        z = Math.round(order*300 - 1400);
-        style.opacity=1;
-        style.transform="translate3d(0px, 0px, " + z +"px)";
-      }else if (order<6) {
-        //scale3d + translateZ + opacity
-        scaleX = scaleY = (order-5)*0.2 + 1;
-        // scaleZ += dz*0.5/50;
-        z=Math.round((order-5)*150 + 100);
-        y=Math.round((order-5)*260);
-        // opacity
-
-        opacity=1-((order-5)*1.5);
-        // if(opacity <= 0){
-        //   console.log("z position: "+ z);
-        // }
-        // set style
-        style.opacity=opacity;
-        style.transform="translate3d(0px, "+ y +"px, " + z +"px) "+
-                             "scale3d(" + scaleX +", "+scaleY+", "+scaleZ +")";
-      }else {
-        z = Math.round((order-6)*100 + 250);
-        style.opacity=0;
-        style.transform="translate3d(0px, "+ y +"px, " + z +"px) "+
-                             "scale3d(" + scaleX +", "+scaleY+", "+scaleZ +")";
-      }
-
-      newOrders.push(order);
-    }
   },
   render:function(){
     var collections=[];
     for (var i=0; i<7; i++) {
-        var layer = "card layer-"+i;
-        var key="layer-"+i;
+        var j=(i+this.state.scrollVertical+7)%7; // scroll pointer
+        var layer = "card layer-"+j;
+        var key="layer-"+j;
         var src=videoJSON[i].SRC;
         var poster=videoJSON[i].Poster;
         if("video"==this.props.catergory){
           collections.push(
-            <VideoCard layer={layer} key={key} src={src} poster={poster} order={this.state.orders[i]} style={this.state.styles[i]}/>
+            <VideoCard layer={layer} key={key} src={src} poster={poster}/>
           );
         }else{
           collections.push(
