5eec49034090bfe0dc008784c1a71bfaffd5cb94
iq72 <liukyzy008@gmail.com>
2018-12-11 19:07:16 +0800

    typo

--- immersive/routes/react.jsx
+++ immersive/routes/react.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var HelloMessage = React.createClass({
   render:function(){
-    return <h1> Hello {this.props.name} <h1>;
+    return <h1> Hello {this.props.name} </h1>;
   }
 });
 
